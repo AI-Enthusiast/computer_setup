96a0a2978bfea146c8f84b75245cf0cdfdc09bc1
Cormac <cormacdacker@gmail.com>
2025-01-13 09:57:03 -0800

    Added TeX Live install

---
+++ install_tex-live.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set working directory
+cd /tmp
+
+# Download the TeX Live installer
+curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+
+# Extract the installer
+zcat < install-tl-unx.tar.gz | tar xf -
+
+# Change to the installer directory
+cd install-tl-*
+
+# Run the installer non-interactively
+sudo perl ./install-tl --no-interaction
+
+# Add TeX Live to the PATH
+echo "export PATH=/usr/local/texlive/2024/bin/x86_64-linux:\$PATH" >> ~/.profile
+source ~/.profile
